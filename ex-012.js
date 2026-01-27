cc0da8b65bfc047713cc60fe4ff4859ff5a6bd57
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-01-27 22:57:20 +0600

    4th commit

---
+++ ex-012.js
@@ -0,0 +1,17 @@
+const arr =[];
+arr.push(10);
+arr.push(20);
+arr.push(30);
+arr.push(40);
+arr.push(50);
+
+const str = ['bubby', 'nini', 'komb'];
+
+
+// console.log(arr);
+// console.log(str[0]);
+
+const numbers=[1,3,6,5,3,4,56,7,0,8,5,43,12];
+let x = numbers.unshift(19);
+console.log(x);
+
---
+++ ex-013.js
@@ -0,0 +1,17 @@
+const str = ['bubby', 'nini', 'komb', 'nico'];
+const nicknames = ['boblin', 'bubba', 'nata', 'bazu', 'cozu', 'bobber', 'bubbies', 'bubby', 'bauma' , 'bubna'];
+
+console.log(nicknames.includes('bubby'));
+
+
+if(nicknames.includes('bobber'))
+{
+    console.log('la bobber catto');
+}
+else{
+    console.log('bobbara');
+}
+
+// console.log(nicknames.ind('bubba'));
+// console.log(Array.isArray(nicknames));
+console.log(nicknames.join('+'));
