d115336049073b81df629a28a595af94680d8385
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-01-29 12:29:30 +0600

    7th commit

--- ex-016.js
+++ ex-016.js
@@ -1,17 +1,17 @@
 const school = "    ABCD    ";
 const schl = "abcdefghijklmnopqrstwxyz";
-// console.log(schl.toUpperCase());
-// console.log(school.toLowerCase());
-// console.log(school.trim());
+console.log(schl.toUpperCase());
+console.log(school.toLowerCase());
+console.log(school.trim());
 
 
 const str0 = `abcdiyryfgybhdbvfbvubhdvbfvhk`;
 
-// const part = str0.slice(2,6);
-// const sp = str0.split('b');
-// console.log(sp);
-// const spl = sp.join('b');
-// console.log(spl);
+const part = str0.slice(2,6);
+const sp = str0.split('b');
+console.log(sp);
+const spl = sp.join('b');
+console.log(spl);
 
 const full = school.concat(schl);
 console.log(full);
---
+++ ex-017.js
@@ -0,0 +1,18 @@
+const sentence = "I am learning web dev" ;
+
+// for(let letter of sentence)
+// {
+//     console.log(letter);
+// }
+
+
+
+// let reverse = '';
+// for(let letter of sentence)
+// {
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+const reversed = sentence.split('').reverse().join('');
+console.log(reversed);
