ac5dc9d2f3f282b77477f5f49edeaf242a9a61b6
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-01-29 15:38:36 +0600

    8th commit

---
+++ ex-018.js
@@ -0,0 +1,9 @@
+const bottle = {
+    brand: "apple",
+    size: "500ml",
+    product: "juice",
+    isHealthy: true,
+    price: 30
+ }
+
+ console.log(bottle['price']);
---
+++ ex-019.js
@@ -0,0 +1,18 @@
+// const friends = ['a', 'b', 'c', 'd']
+
+const arr = [1,1,2,3,4,5,6,7,8,9,3];
+// console.log(arr.reverse());
+
+const arr2 = [];
+j = arr.length - 1;
+
+// for(let i=0; i<arr.length; i++){
+//     arr2[j] = arr[i];
+//     j--;
+// }
+// 
+
+for (let x of arr){
+    arr2.unshift(x);
+}
+console.log(arr2);
